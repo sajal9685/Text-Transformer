6387ea3c3233537b86cff77b9bae6c7aeab7f79e
Sajal Chaturvedi <sajalchaturvedi8@gmail.com>
2025-05-09 16:33:21 +0530

    cropping feature

--- src/component/cropImage.js
+++ src/component/cropImage.js
@@ -97,5 +97,122 @@
 //       </div>
 //     );
 //   }
+//   import React, { useState } from "react";
+//   import Cropper from 'react-easy-crop'
+//   export default function ImageUpload() {
+//       const [file , setFile]=useState()
+//       const [crop, setCrop] = useState({ x: 0, y: 0 })
+//       const [zoom, setZoom] = useState(1)
+//       const handleImageUpload =(event) =>{
+//           setFile(URL.createObjectURL( event.target.files[0]))
+//       }
+      
+//       const handleImageClick =(croppedArea, croppedAreaPixels) =>{
+//           console.log(croppedArea, croppedAreaPixels)
+//       }
+//    return (
+//       <div className="file-upload">
+//           <div className="upload-container">
+//               <div className="upload-icon">
+//                   <img width={240} height={240} src ={file} alt=""/>
+//                   <Cropper
+//         image={file}
+//         crop={crop}
+//         zoom={zoom}
+//         aspect={4 / 3}
+//         onCropChange={setCrop}
+//         onCropComplete={handleImageClick}
+//         onZoomChange={setZoom}
+//       />
+//               </div>
+//               <input 
+//               type="file"
+//               className="file-input"
+//               onChange={handleImageUpload}>
   
+//               </input>
+//           </div>
+//           </div>
+//    );
+//   }
+//   import react, {useRef, useState } from "react";
+// import ReactCrop from "react-image-crop";
+// import "react-image-crop/dist/ReactCrop.css";
+// const ImageUpload = () => {
+//   const [src, selectFile] = useState(null);
+//   const [crop, setCrop] = useState({
+//     unit: 'px',
+//     x: 0,
+//     y: 0,
+//     width: 100,
+//     height: 100,
+//     aspect: 16 / 9,
+//   });
   
+//   const [image, setImage] = useState(null);
+//   const [result, setResult]=useState(null);
+//   const imgRef = useRef(null);
+//   const handleFileChange = (e) => {
+//     const file = e.target.files[0];
+//     if (file) {
+//       const imageUrl = URL.createObjectURL(file);
+//       console.log("Selected Image URL:", imageUrl);
+//       selectFile(imageUrl);
+//     }
+//   };
+//   function getCroppedImage() {
+//     const canvas = document.createElement("canvas");
+//     const scaleX=image.naturalWidth /image.width;
+//     const scaleY=image.naturalHeight /image.height;
+//     canvas.width = crop.width;
+//     canvas.height = crop.height;
+//     const ctx = canvas.getContext("2d");
+
+//     ctx.drawImage(
+//     imgRef.current,
+//       crop.x *scaleX,
+//       crop.y * scaleY,
+//       crop.width*scaleX,
+//       crop.height*scaleY,
+//       0,
+//       0,
+//       crop.width,
+//       crop.height
+//     );
+// const base64Image =canvas.toDataURL('image/jpeg');
+//             setResult(base64Image)
+//   }
+//   const onImageLoaded = (img) => {
+//     imgRef.current = img;
+//   };
+//   return (
+//     <div className="container">
+//       <div className="row">
+//         <div className="col-6">
+//           <input type="file" accept="image/*" onChange={handleFileChange} />
+//         </div>
+
+//         {src && 
+//           <div className="col-6">
+           
+//             <ReactCrop
+//               src={src}
+//               onImageLoaded={onImageLoaded}
+//               crop={crop}
+//               onChange={(newCrop) => setCrop(newCrop)}
+//             />
+//             <button className="btn btn-danger" onClick={getCroppedImage}>
+//               CropImage
+//             </button>
+//           </div>
+//         }
+//         {result && <div className="col-6">
+//             <img src={result} alt="crop image"className="img-fluid"/>
+//         </div>
+//             }
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default ImageUpload;
